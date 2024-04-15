0ada5a87fc4310e31035e09f6621caa861d6660e
Greg <gt@gregorytesto.com>
2024-04-15 15:43:39 -0400

    finished adding menu and basic calc

--- workbooks/week2/MathCalculator_CodeAlong/src/main/java/com/ps/Main.java
+++ workbooks/week2/MathCalculator_CodeAlong/src/main/java/com/ps/Main.java
@@ -3,17 +3,65 @@ package com.ps;
 import java.util.*;
 
 class Main {
-    public static void main(String[] args){
+
+    static Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
         System.out.println("Which calculator would you like to use? (b)asic or (s)cientific?");
-        Scanner scanner = new Scanner(System.in);
         String command = scanner.nextLine();
 
-        if(command.equalsIgnoreCase("b")){
-            System.out.println("Run basic calculator");
-        } else if(command.equalsIgnoreCase("s")){
-            System.out.println("Run scientific calculator");
-        } else {
-            System.out.println("Command not found");
+        switch (command.toLowerCase()) {
+            case "b":
+                runBasicCalculator();
+                break;
+            case "s":
+                runScientificCalculator();
+                break;
+            default:
+                System.out.println("Command not found");
         }
+    }
+
+    public static void runBasicCalculator() {
+        System.out.println("Run basic calculator");
+        // Operation
+        // num1
+        // num2
+        System.out.println("What operation would you like to perform?");
+        System.out.println("1: addition \n2: subtraction \n3: multiplication \n4: divide \n5: modulo");
+
+        int operation = scanner.nextInt();
+
+        System.out.println("What the first number?");
+        int numberOne = scanner.nextInt();
+
+        System.out.println("What the second number?");
+        int numberTwo = scanner.nextInt();
+
+        System.out.println("Op: " + operation + ", num1 " + numberOne + ", num2 " + numberTwo);
+
+        switch (operation) {
+            case 1:
+                System.out.println("Your result is: " + (numberOne + numberTwo));
+                break;
+            case 2:
+                System.out.println("Your result is: " + (numberOne - numberTwo));
+                break;
+            case 3:
+                System.out.println("Your result is: " + (numberOne * numberTwo));
+                break;
+            case 4:
+                System.out.println("Your result is: " + (numberOne / numberTwo));
+                break;
+            case 5:
+                System.out.println("Your result is: " + (numberOne % numberTwo));
+                break;
+            default:
+                System.out.println("Operation not found");
+        }
+
+    }
+
+    public static void runScientificCalculator() {
+        System.out.println("Run scientific calculator");
     }
 }
