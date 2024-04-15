269b1f7fe7bca1d896252d10ca9f836209ee4ac9
Greg <gt@gregorytesto.com>
2024-04-15 14:27:58 -0400

    added menu

--- workbooks/week2/MathCalculator_CodeAlong/src/main/java/com/ps/Main.java
+++ workbooks/week2/MathCalculator_CodeAlong/src/main/java/com/ps/Main.java
@@ -1,6 +1,19 @@
 package com.ps;
+
+import java.util.*;
+
 class Main {
     public static void main(String[] args){
-        System.out.println("Hello World");
+        System.out.println("Which calculator would you like to use? (b)asic or (s)cientific?");
+        Scanner scanner = new Scanner(System.in);
+        String command = scanner.nextLine();
+
+        if(command.equalsIgnoreCase("b")){
+            System.out.println("Run basic calculator");
+        } else if(command.equalsIgnoreCase("s")){
+            System.out.println("Run scientific calculator");
+        } else {
+            System.out.println("Command not found");
+        }
     }
 }
