380e0ba454332d4e705215d7c0cf3fb9ef41b682
Greg <gt@gregorytesto.com>
2024-04-17 14:21:58 -0400

    added Car class

--- workbooks/week2/Car_Playground/src/main/java/com/ps/Car.java
+++ workbooks/week2/Car_Playground/src/main/java/com/ps/Car.java
@@ -1,13 +1,85 @@
 package com.ps;
 
 public class Car {
-    // make - String
-    // model - String
-    // price - float
-    // numOfSales - int
-    // isNew - boolean
+    private String make;
+    private String model;
+    private float price;
+    private int numOfSales;
+    private boolean isNew;
 
-    // Constructor
+    public Car(String make, String model, float price, int numOfSales, boolean isNew) {
+        this.make = make;
+        this.model = model;
+        this.price = price;
+        this.numOfSales = numOfSales;
+        this.isNew = isNew;
+    }
 
-    // Getters and setters
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public int getNumOfSales() {
+        return numOfSales;
+    }
+
+    public void setNumOfSales(int numOfSales) {
+        this.numOfSales = numOfSales;
+    }
+
+    public boolean isNew() {
+        return isNew;
+    }
+
+    public void setIsNew(boolean isNew) {
+        this.isNew = isNew;
+    }
 }
+
+
+
+//    // Constructor method signs
+//    // Capitalized
+//    // It has the same name of the class
+//    // There is no return type
+//    // This will run once when you instantiate(new) the class
+//    public Car(String make, String model){
+//        this.make = make;
+//        this.model = model;
+//    }
+//
+//    // Getters and setters
+//
+//    // Follows the naming convention get____(Name of the property)
+//    // Empty parameters
+//    // Return type of the that property
+//    // Return this.___(Name of the property)
+//    public String getMake(){
+//        return this.make;
+//    }
+//    // Follows the naming convention set____(Name of the property)
+//    // Must receive new value for property matching the type
+//    // Return nothing
+//    public void setMake(String make){
+//        this.make = make;
+//    }
