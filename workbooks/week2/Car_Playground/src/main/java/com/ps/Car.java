725a3c4afd8b07c4aa75c88e4ec55554f82f690c
Greg <gt@gregorytesto.com>
2024-04-17 13:24:37 -0400

    added car playground project

---
+++ workbooks/week2/Car_Playground/src/main/java/com/ps/Car.java
@@ -0,0 +1,13 @@
+package com.ps;
+
+public class Car {
+    // make - String
+    // model - String
+    // price - float
+    // numOfSales - int
+    // isNew - boolean
+
+    // Constructor
+
+    // Getters and setters
+}
