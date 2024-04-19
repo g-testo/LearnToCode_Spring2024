bc79173cdcef4d13a2810090144c7de5292a3911
Greg <gt@gregorytesto.com>
2024-04-19 11:21:04 -0400

    added workbook two starter file

--- workshops/WorkshopTwo_NeighborhoodLibrary/src/main/java/com/ps/Main.java
+++ workshops/WorkshopTwo_NeighborhoodLibrary/src/main/java/com/ps/Main.java
@@ -3,5 +3,26 @@ package com.ps;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+
+        // Store Home Screen
+        do {
+
+            // Show Available books
+            do{
+
+            } while();
+
+            // Show Checked out books
+            do{
+
+                // Check in book
+                do{
+
+                } while();
+
+            } while();
+
+        }while();
     }
 }
