6421f70cf4d6bb7e15aa98f938c78679d261612c
Greg <gt@gregorytesto.com>
2024-04-22 13:43:06 -0400

    added Exception class project

---
+++ workbooks/week3/ExceptionPlayground/src/main/java/com/ps/Main.java
@@ -0,0 +1,21 @@
+package com.ps;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String[] names = {"Ezra", "Elisha", "Ian", "Siddalee", "Pursalane", "Zephaniah"};
+        System.out.print("Pick a kid (select #1 - #6): ");
+        int index = scanner.nextInt();
+        index--;
+
+        try {
+            System.out.println(names[index]);
+        } catch(Exception e){
+            System.out.println(e);
+        }
+
+        System.out.println("Rest of the program");
+    }
+}
