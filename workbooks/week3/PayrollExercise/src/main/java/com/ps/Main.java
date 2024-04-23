b799fbdff00f1a3187e410cb0e20779381580794
Greg <gt@gregorytesto.com>
2024-04-23 11:07:08 -0400

    finished the payroll exercise

--- workbooks/week3/PayrollExercise/src/main/java/com/ps/Main.java
+++ workbooks/week3/PayrollExercise/src/main/java/com/ps/Main.java
@@ -2,6 +2,8 @@ package com.ps;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.util.Arrays;
+import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) {
@@ -10,7 +12,20 @@ public class Main {
 
             String line;
             while((line = bufReader.readLine()) != null){
-                System.out.println(line);
+                String[] splitLine = line.split("\\|");
+
+                long id = Long.parseLong(splitLine[0]);
+                String name = splitLine[1];
+                float hoursWorked = Float.parseFloat(splitLine[2]);
+                float payRate = Float.parseFloat(splitLine[3]);
+
+                Employee employee = new Employee(id, name, hoursWorked, payRate);
+
+                System.out.printf("Employee ID: %d, Name: %s, Gross Pay: %.2f \n",
+                        employee.getEmployeeId(),
+                        employee.getName(),
+                        employee.calcGrossPay()
+                );
             }
 
         } catch(Exception e){
