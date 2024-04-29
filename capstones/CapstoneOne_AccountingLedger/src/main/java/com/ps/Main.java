fef1b3dc64a9399d8146768ca461e1c59e48d0ec
Greg <gt@gregorytesto.com>
2024-04-29 16:59:43 +0200

    Added notes to Capstone One

--- capstones/CapstoneOne_AccountingLedger/src/main/java/com/ps/Main.java
+++ capstones/CapstoneOne_AccountingLedger/src/main/java/com/ps/Main.java
@@ -1,7 +1,45 @@
 package com.ps;
 
 public class Main {
+    // Create a static variable to store transactions: hint- ArrayList<Transaction>
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Create class for storing transactions
+            // Fields include date, time, description, vendor and amount
+            // *Note
+                // if amount is positive then the amount is a deposit
+                // if amount is negative then the amount is a payment
+
+        // Load records of transactions to the static variable for transactions
+            // * hint-Load transactions before the menu loop
+
+        // Create CLI menu items
+            // *hint
+            // first do-while: Home menu
+            // second do-while: Ledger menu
+            // third do-while: Reports menu
+
+        // Perform deposit and payment transactions while recording the transactions to the file
+
+        // Reports
+
+        // Menu Outline
+        // **Home Menu**
+        // D) Add Deposit
+        // P) Make Payment(Debit)
+        // L) Ledger
+            // **Ledger Menu**
+            // A) All
+            // D) Deposit
+            // P) Payments
+            // R) Reports
+                // R) **Reports Menu**
+                // 1) Month To Date
+                // 2) Previous Month
+                // 3) Year To Date
+                // 4) Previous Year
+                // 5) Search by Vendor
+                // 0) Back
+            // H) Home
+        // X) Exit
     }
 }
