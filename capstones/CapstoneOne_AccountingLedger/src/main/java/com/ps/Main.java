0fd6823731dbc76f6014ed79d8d6bb7155990a7a
Greg <gt@gregorytesto.com>
2024-04-30 16:25:38 +0200

    added code from a in-class report demo

--- capstones/CapstoneOne_AccountingLedger/src/main/java/com/ps/Main.java
+++ capstones/CapstoneOne_AccountingLedger/src/main/java/com/ps/Main.java
@@ -1,5 +1,9 @@
 package com.ps;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.ArrayList;
+
 public class Main {
     // Create a static variable to store transactions: hint- ArrayList<Transaction>
     public static void main(String[] args) {
@@ -41,5 +45,54 @@ public class Main {
                 // 0) Back
             // H) Home
         // X) Exit
+
+
+        // ** Report Demo **
+        //  date, time, description, vendor and amount
+        Transaction transaction1 = new Transaction(
+                LocalDate.of(2024, 3, 14),
+                LocalTime.of(14, 43, 05, 0),
+                "Monstera Plant",
+                "Amazon",
+                39.99f
+        );
+
+        Transaction transaction2 = new Transaction(
+                LocalDate.of(2024, 4, 29),
+                LocalTime.of(8, 05, 25, 0),
+                "HeartLeaf Plant",
+                "Amazon",
+                19.99f
+        );
+
+        ArrayList<Transaction> transactions = new ArrayList<>();
+        transactions.add(transaction1);
+        transactions.add(transaction2);
+
+        // Generate a report- Month to Date
+
+        for(Transaction transaction : transactions){
+            LocalDate transactionDate = transaction.getDate();
+            LocalDate nowDate = LocalDate.now();
+            if(transactionDate.getMonthValue() == nowDate.getMonthValue()){
+                System.out.println("             Month To Date Report          ");
+                System.out.println("              -- Transactions --           ");
+                System.out.println("Date - Time - Description - Vendor - Amount");
+                System.out.printf("%s - %s - %s - %s - $%.2f",
+                        transaction.getDate(),
+                        transaction.getTime(),
+                        transaction.getDescription(),
+                        transaction.getVendor(),
+                        transaction.getAmount()
+                );
+
+                //            Month To Date Report
+                //            -- Transactions --
+                // Date - Time - Description - Vendor - Amount
+                // 4-29-24 - 8:05 - HeartLeaf Plant - Amazon - $19.99
+
+            }
+        }
+
     }
 }
