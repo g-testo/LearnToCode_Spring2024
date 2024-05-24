8cc3c8bab48849c01b24cb872043eb7dcb27cbaf
Greg <gt@gregorytesto.com>
2024-05-24 14:27:04 -0400

    added multi spiral example

--- workbooks/week7/TurtlePaintForLife/src/main/java/com/ps/Main.java
+++ workbooks/week7/TurtlePaintForLife/src/main/java/com/ps/Main.java
@@ -1,31 +1,50 @@
 package com.ps;
 
 import java.awt.*;
+import java.util.ArrayList;
 
 public class Main {
+    private static World world = new World(600, 600, Color.BLACK);
     public static void main(String[] args) {
-        World world = new World(600, 600, Color.BLACK);
-        Turtle turtle = new Turtle(world, 0, 0);
+//        Turtle turtle = new Turtle(world, 0, 0);
+//        turtle.setPenWidth(1);
+//        turtle.setDelay(.1);
+//        singleSpiral(turtle);
 
+        multiSpiral();
+    }
+
+    public static void multiSpiral(){
+        int width = 150;
+        int[][] coordinates = {{-width, -width},{width,-width},{width,width},{-width,width}};
+        ArrayList<Turtle> turtles = new ArrayList<>();
+
+        for(int[] coordinate: coordinates){
+            Turtle turtle = new Turtle(world, coordinate[0], coordinate[1]);
+
+            turtle.setShellSize(0);
+            turtle.setPenWidth(1);
+            turtle.setDelay(.01);
+
+            turtles.add(turtle);
+        }
+
+        for(Turtle turtle: turtles){
+            world.addTurtle(turtle);
+            singleSpiral(turtle);
+        }
+    }
+
+    public static void singleSpiral(Turtle turtle){
         Color[] colors = {Color.red,Color.BLUE, Color.GREEN, Color.PINK};
 
-//        0 : 1 5
-//        1 : 2 6
-//        2 : 3 7
-//        3 : 4 8
         int accum = 0;
-        for(int i=1;i<30;i++){
-            int randomSize = (int) ((Math.random() * (6 - 1)) + 1);
-            turtle.setPenWidth(randomSize);
-
+        for(int i=1;i<25;i++){
             int colorIndex = (i-1)%4;
-
             turtle.setColor(colors[colorIndex]);
-
             turtle.forward(accum);
             turtle.turnRight(90);
             accum += i;
         }
-
     }
 }
