8f9f78e8900769fbe71590cf3f5388cbcead03eb
Greg <gt@gregorytesto.com>
2024-06-12 14:44:49 -0400

    renamed and tested UserDAO class

--- workbooks/week10/TicketPlayground/src/main/java/com/ps/UserDAO.java
+++ workbooks/week10/TicketPlayground/src/main/java/com/ps/UserDAO.java
@@ -6,26 +6,36 @@ import org.apache.commons.dbcp2.BasicDataSource;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
-public class TicketPlaygroundDataManager {
+public class UserDAO {
     private BasicDataSource dataSource;
 
-    public TicketPlaygroundDataManager(BasicDataSource basicDataSource){
+    public UserDAO(BasicDataSource basicDataSource){
         this.dataSource = basicDataSource;
     }
 
     public List<User> getAllUsers(){
+        List<User> users = new ArrayList<>();
         try(
                 Connection connection = dataSource.getConnection();
                 PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM user");
                 ResultSet resultSet = preparedStatement.executeQuery();
         ){
             while(resultSet.next()){
-                System.out.println(resultSet.getString("first_name"));
+                int id = resultSet.getInt("id");
+                String firstName = resultSet.getString("first_name");
+                String lastName = resultSet.getString("last_name");
+
+                User user = new User(id, firstName, lastName);
+                users.add(user);
             }
         }
         catch(Exception e){
             e.printStackTrace();
+        } finally {
+            return users;
         }
     }
 }
--- workbooks/week10/TicketPlayground/src/main/java/com/ps/UserInterface.java
+++ workbooks/week10/TicketPlayground/src/main/java/com/ps/UserInterface.java
@@ -1,12 +1,15 @@
 package com.ps;
 
+import com.ps.models.User;
 import org.apache.commons.dbcp2.BasicDataSource;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.List;
 
 public class UserInterface {
+    private static UserDAO userDAO;
 
     public static void init(String[] args){
         BasicDataSource basicDataSource = new BasicDataSource();
@@ -14,12 +17,13 @@ public class UserInterface {
         basicDataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
         basicDataSource.setUsername(args[0]);
         basicDataSource.setPassword(args[1]);
-
-
+        userDAO = new UserDAO(basicDataSource);
     }
 
     public static void display(String[] args){
         init(args);
 
+        List<User> users = userDAO.getAllUsers();
+        System.out.println(users);
     }
 }
