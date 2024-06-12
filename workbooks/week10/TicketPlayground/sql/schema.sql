65d47012625c24b21cf79ea4851096fdcb3b5981
Greg <gt@gregorytesto.com>
2024-06-12 11:11:53 -0400

    added user seed data:

--- workbooks/week10/TicketPlayground/sql/schema.sql
+++ workbooks/week10/TicketPlayground/sql/schema.sql
@@ -11,6 +11,14 @@ CREATE TABLE `user` (
 	PRIMARY KEY (`id`)
 );
 
+CREATE TABLE `ticket` (
+	`id` INTEGER NOT NULL AUTO_INCREMENT,
+	`event_name` VARCHAR(50) NOT NULL,
+	`price` FLOAT NOT NULL,
+    `type` VARCHAR(50) NOT NULL,
+	PRIMARY KEY (`id`)
+);
+
 CREATE TABLE `order` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `user_id` INTEGER NOT NULL,
@@ -22,11 +30,22 @@ CREATE TABLE `order` (
 	PRIMARY KEY (`id`)
 );
 
-CREATE TABLE `ticket` (
-	`id` INTEGER NOT NULL AUTO_INCREMENT,
-	`event_name` VARCHAR(50) NOT NULL,
-	`price` FLOAT NOT NULL,
-    `type` VARCHAR(50) NOT NULL,
-	PRIMARY KEY (`id`)
-)
-
+-- Seed 
+INSERT INTO user(`first_name`, `last_name`) VALUES('Michael', 'Johnson');
+INSERT INTO user(`first_name`, `last_name`) VALUES('Emily', 'Brown');
+INSERT INTO user(`first_name`, `last_name`) VALUES('David', 'Williams');
+INSERT INTO user(`first_name`, `last_name`) VALUES('Sarah', 'Jones');
+INSERT INTO user(`first_name`, `last_name`) VALUES('James', 'Garcia');
+INSERT INTO user(`first_name`, `last_name`) VALUES('Olivia', 'Martinez');
+INSERT INTO user(`first_name`, `last_name`) VALUES('Robert', 'Rodriguez');
+INSERT INTO user(`first_name`, `last_name`) VALUES('Sophia', 'Hernandez');
+INSERT INTO user(`first_name`, `last_name`) VALUES('William', 'Lopez');
+INSERT INTO user(`first_name`, `last_name`) VALUES('Ava', 'Gonzalez');
+INSERT INTO user(`first_name`, `last_name`) VALUES('Daniel', 'Wilson');
+INSERT INTO user(`first_name`, `last_name`) VALUES('Mia', 'Anderson');
+INSERT INTO user(`first_name`, `last_name`) VALUES('Matthew', 'Thomas');
+INSERT INTO user(`first_name`, `last_name`) VALUES('Abigail', 'Taylor');
+INSERT INTO user(`first_name`, `last_name`) VALUES('Joseph', 'Moore');
+INSERT INTO user(`first_name`, `last_name`) VALUES('Isabella', 'Jackson');
+INSERT INTO user(`first_name`, `last_name`) VALUES('Charles', 'Martin');
+INSERT INTO user(`first_name`, `last_name`) VALUES('Charlotte', 'Lee');
