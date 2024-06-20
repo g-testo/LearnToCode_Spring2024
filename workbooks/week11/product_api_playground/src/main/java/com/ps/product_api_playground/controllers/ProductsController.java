8db4fbc1ac0311e702cb2bc8baa499853e9df731
Greg <gt@gregorytesto.com>
2024-06-20 15:52:12 -0400

    added status codes

--- workbooks/week11/product_api_playground/src/main/java/com/ps/product_api_playground/controllers/ProductsController.java
+++ workbooks/week11/product_api_playground/src/main/java/com/ps/product_api_playground/controllers/ProductsController.java
@@ -2,6 +2,7 @@ package com.ps.product_api_playground.controllers;
 
 import com.ps.product_api_playground.models.Product;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import com.ps.product_api_playground.data.mysql.MySqlProductsDao;
@@ -18,6 +19,7 @@ public class ProductsController {
 
     // Filter product by name /products?name=fa
     @GetMapping("/products")
+    @ResponseStatus(value= HttpStatus.OK)
     public List<Product> getProducts(
             @RequestParam(required = false) String name,
             @RequestParam(required = false) int categoryId
@@ -26,21 +28,25 @@ public class ProductsController {
     }
 
     @GetMapping("/products/{id}")
+    @ResponseStatus(value= HttpStatus.OK)
     public Product getProduct(@PathVariable int id){
         return mySqlProductsDao.getProductById(id);
     }
 
     @PostMapping("/products")
+    @ResponseStatus(value = HttpStatus.CREATED)
     public Product postProduct(@RequestBody Product product){
         return mySqlProductsDao.createProduct(product);
     }
 
     @PutMapping("/products/{id}")
+    @ResponseStatus(value= HttpStatus.OK)
     public void putProduct(@PathVariable int id, @RequestBody Product product){
         mySqlProductsDao.updateProduct(id, product);
     }
 
     @DeleteMapping("/products/{id}")
+    @ResponseStatus(value= HttpStatus.NO_CONTENT)
     public void removeProduct(@PathVariable int id){
         mySqlProductsDao.deleteProduct(id);
     }
